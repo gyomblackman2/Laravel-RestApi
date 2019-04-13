288ce1c14ec7567572c96c48b6c2e5e89e48f17a
Tatenda Kwaramba <kwarambaandy@gmail.com>
2019-04-13 04:38:34 +0200

    created models for product and reviews

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products', 'ProductController');
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews', 'ReviewsController');
+});
